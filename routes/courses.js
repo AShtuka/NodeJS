43341deb394d187d6e91c85f6e2743233a78c6ab
Oleksandr Shtuka <ashtuka@gmail.com>
2020-01-26 18:30:41 +0200

    start create courses app

---
+++ routes/courses.js
@@ -0,0 +1,10 @@
+const {Router} = require('express');
+const Course = require('../models/course')
+const router = Router();
+
+router.get('/', async (req, res, next) => {
+    const courses = await Course.getAll();
+    res.render('courses', {title : 'Courses page', isCourses : true, courses});
+});
+
+module.exports = router;
