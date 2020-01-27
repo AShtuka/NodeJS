367c080799f6e8887d60ebf212d68a0e9d236709
Oleksandr Shtuka <ashtuka@gmail.com>
2020-01-27 11:43:26 +0200

    countinue dev my app

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars');
 const homeRoutes = require('./routes/home');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
+const cartRoutes = require('./routes/cart');
 
 const app = express();
 const hbs = exphbs.create({defaultLayout: 'main', extname: 'hbs'});
@@ -11,10 +12,11 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
 app.use('/', homeRoutes);
 app.use('/add', addRoutes);
 app.use('/courses', coursesRoutes);
+app.use('/cart', cartRoutes);
 
 const PORT = process.env.PORT || 3000;
 
--- routes/courses.js
+++ routes/courses.js
@@ -7,4 +7,23 @@ router.get('/', async (req, res, next) => {
     res.render('courses', {title : 'Courses page', isCourses : true, courses});
 });
 
+router.get('/:id/edit', async (req, res) => {
+   if (!req.query.allow) {
+       return res.redirect('/');
+   };
+   const course = await Course.getById(req.params.id);
+   res.render('course-edit', {title: `Edit ${course.title}`, course})
+});
+
+router.get('/:id', async (req, res, next) => {
+    const course = await Course.getById(req.params.id);
+    res.render('course', {layout: 'empty', title: `Course: ${course.title}`, course});
+});
+
+router.post('/edit', async (req, res) => {
+   await Course.update(req.body);
+   res.redirect('/courses');
+});
+
+
 module.exports = router;
